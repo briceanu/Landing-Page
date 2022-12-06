902b10fabd6f557a94788ff7dca1c882a24d601d
Teodor <ursus_24@yahoo.com>
2022-12-06 20:59:13 +0200

    nav and header made and some styles add to them

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import style from './style.scss';
+import dog from './assets/dog.svg';
+import dog_640 from './assets/dog_640.jpg';
+import dog_1280 from './assets/dog_1280.jpg';
+import dog_1920 from './assets/dog_1920.jpg';
+console.log('aowd');
